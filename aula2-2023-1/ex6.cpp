0643156356f14ae28f58fb415fd9dc3a6b9ed5fd
Vitor Silveira <102565091+VtSilveira@users.noreply.github.com>
2023-07-10 10:22:33 -0300

    Create ex6.cpp

---
+++ aula2-2023-1/ex6.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+bool solve(int mid,vector<int>&v){
+    vector<int>res;
+    int val=v[0]-mid;
+    res.push_back(val);
+    for(int i=1;i<v.size();i++){
+        if(abs(val-v[i])>mid){
+            val=v[i]-mid;
+            res.push_back(val);
+        }
+    }
+
+   if( res.size()<=3){
+    return true;
+   }
+   return false;
+}
+signed main(){
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+  
+int t;cin>>t;
+while ( t--)
+{
+     int n;cin>>n;
+     vector<int>v(n);
+     forn(i,n){
+        cin>>v[i];
+     }
+     sort(v.rbegin(),v.rend());
+     int lo=0;
+     int hi=1e9;
+     int ans=1e9;
+ 
+     while ( hi>=lo)
+     {
+        int mid=(lo+hi)/2;
+        
+        if(solve(mid,v)){
+            ans=mid;
+            hi=mid-1;
+        }
+        else
+        {
+              lo=mid+1;
+        }
+     }
+     cout<<ans<<endl;
+     }
+}
